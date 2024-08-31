780b7820015de01fa8d6fc9a17a04e8bc4e19484
Your Name <99302865+EthanCui2008@users.noreply.github.com>
2024-08-30 18:25:24 -0700

    Cleaning up the code, going to remake later

--- CNUMPP_H.h
+++ CNUMPP_H.h
@@ -9,8 +9,8 @@
 #include "STLREADER_H.h"
 #include "Point3D.h"
 
-//probably should change such that it returns the 2d matrix itself, sadly I do not understand pointers very well
-//Matrix Multiplication Function
+
+//matrix multiplication function, inputs: 2 2d arrays, and corresponding row and column integers, along with an output array
 void matmul(float* matrix1, int rows1, int cols1, float* matrix2, int rows2, int cols2, float* result) {
     if (cols1 != rows2) {
         std::cout << "Dumbass" << std::endl;
@@ -27,6 +27,7 @@ void matmul(float* matrix1, int rows1, int cols1, float* matrix2, int rows2, int
     }
 }
 
+//copies one 2d array to an other given same row and int size, inputs: 2 2d arrays, correspond row and column sizes
 void copy(float* source, float* destination, int rows, int cols) {
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
@@ -35,13 +36,15 @@ void copy(float* source, float* destination, int rows, int cols) {
     }
 }
 
-void matmul_modify(float* original, int rows_o, int cols_o, float* modifier, int rows_m, int cols_m) {
+//directly modifies 2d array via matmul with second 2d array
+void matmulModify(float* original, int rows_o, int cols_o, float* modifier, int rows_m, int cols_m) {
     float original_copy[rows_o][cols_o];
     copy(original, *original_copy, rows_o, cols_o);
     matmul(*original_copy, rows_o, cols_o, modifier, rows_m, cols_m, original);
 }
 
-void rotateAroundX(Vector3D& v, float deg) {
+//rotates a vector by deg degrees around the x axis, inputs: vector, float degree
+void vectorRotationX(Vector3D& v, float deg) {
     float theta = deg * (M_PI / 180.0f);
     float cosTheta = std::cos(theta);
     float sinTheta = std::sin(theta);
@@ -51,7 +54,8 @@ void rotateAroundX(Vector3D& v, float deg) {
     v.z = originalY * sinTheta + v.z * cosTheta;
 }
 
-void rotateAroundY(Vector3D& v, float deg) {
+//rotates a vector by deg degrees around the y axis, inputs: vector, float degree
+void vectorRotationY(Vector3D& v, float deg) {
     float theta = deg * (M_PI / 180.0f);
     float cosTheta = std::cos(theta);
     float sinTheta = std::sin(theta);
@@ -61,7 +65,8 @@ void rotateAroundY(Vector3D& v, float deg) {
     v.z = -originalX * sinTheta + v.z * cosTheta;
 }
 
-void rotateAroundZ(Vector3D& v, float deg) {
+//rotates a vector by deg degrees around the z axis, inputs: vector, float degree
+void vectorRotationZ(Vector3D& v, float deg) {
     float theta = deg * (M_PI / 180.0f);
     float cosTheta = std::cos(theta);
     float sinTheta = std::sin(theta);
@@ -71,8 +76,8 @@ void rotateAroundZ(Vector3D& v, float deg) {
     v.y = originalX * sinTheta + v.y * cosTheta;
 }
 
-void xrotate(float* matrix1, int rows1, int xdeg, Vector3D& v){
-    rotateAroundX(v, xdeg);
+void facetRotationX(float* matrix1, int rows1, int xdeg, Vector3D& v){
+    vectorRotationX(v, xdeg);
     float xRad = xdeg * M_PI / 180.0;
     float sinX = std::sin(xRad); float cosX = std::cos(xRad);
     float xRotation[4][4] = {
@@ -81,12 +86,11 @@ void xrotate(float* matrix1, int rows1, int xdeg, Vector3D& v){
         {0, -sinX, cosX, 0},
         {0, 0, 0, 1}
     };
-    matmul_modify(matrix1, rows1, 4, *xRotation, 4, 4);
-    
+    matmulModify(matrix1, rows1, 4, *xRotation, 4, 4);
 }
 
-void yrotate(float* matrix1, int rows1, int ydeg, Vector3D& v){
-    rotateAroundY(v, ydeg);
+void facetRotationY(float* matrix1, int rows1, int ydeg, Vector3D& v){
+    vectorRotationY(v, ydeg);
     float yRad = ydeg * M_PI / 180.0;
     float sinY = std::sin(yRad); float cosY = std::cos(yRad);
 
@@ -96,11 +100,11 @@ void yrotate(float* matrix1, int rows1, int ydeg, Vector3D& v){
         {sinY, 0, cosY, 0},
         {0, 0, 0, 1}
     };
-    matmul_modify(matrix1, rows1, 4, *yRotation, 4, 4);
+    matmulModify(matrix1, rows1, 4, *yRotation, 4, 4);
 }
 
-void zrotate(float* matrix1, int rows1, int zdeg, Vector3D& v){
-    rotateAroundZ(v, zdeg);
+void facetRotationZ(float* matrix1, int rows1, int zdeg, Vector3D& v){
+    vectorRotationZ(v, zdeg);
     float zRad = zdeg * M_PI / 180.0;
     float sinZ = std::sin(zRad); float cosZ = std::cos(zRad);
 
@@ -110,47 +114,62 @@ void zrotate(float* matrix1, int rows1, int zdeg, Vector3D& v){
         {0, 0, 1, 0},
         {0, 0, 0, 1}
     };
-    matmul_modify(matrix1, rows1, 4, *zRotation, 4, 4);
+    matmulModify(matrix1, rows1, 4, *zRotation, 4, 4);
 }
 
-void move(float* matrix1, int rows1, float x, float y, float z){
+//moves matrix of values by corresponding x, y, z, inputs are as such too
+void moveMatrix(float* matrix1, int rows1, float x, float y, float z){
     float move[4][4] = {
         {1, 0, 0, 0},
         {0, 1, 0, 0},
         {0, 0, 1, 0},
         {x, y, z, 1}
     };
-    matmul_modify(matrix1, rows1, 4, *move, 4, 4);
+    matmulModify(matrix1, rows1, 4, *move, 4, 4);
 }
 
-void scale(float* matrix1, int rows1, float x, float y, float z){
+//scales matrix of values by corresponding x, y, z
+void scaleMatrix(float* matrix1, int rows1, float x, float y, float z){
     float scale[4][4] = {
         {x, 0, 0, 0},
         {0, y, 0, 0},
         {0, 0, z, 0},
         {0, 0, 0, 1}
     };
-    matmul_modify(matrix1, rows1, 4, *scale, 4, 4);
+    matmulModify(matrix1, rows1, 4, *scale, 4, 4);
 }
 
-void viewvol(float* matrix1, int rows1, float r, float l, float b, float t, float f, float n){
+//view volume transform, takes all points within bounding box defined by r [right]
+void viewVolumeTransform(float* matrix1, int rows1, Vector3D& bottom_near_left_point, Vector3D& top_far_right_point){
+    
+    int left = bottom_near_left_point.y;
+    int bottom = bottom_near_left_point.z;
+    int near = bottom_near_left_point.x;
+
+    int right =  top_far_right_point.y;
+    int top =  top_far_right_point.z;
+    int far = top_far_right_point.x;
+
     float view[4][4] = {
-        {2/(r-l), 0, 0, 0},
-        {0, 2/(b-t), 0, 0},
-        {0, 0, 2/(f-n), 0},
-        {-1*(r+l)/(r-l), -1*(b+t)/(b-t), -1*n/(f-n), 1}
+        {2/(right-left), 0, 0, 0},
+        {0, 2/(top-bottom), 0, 0},
+        {0, 0, 2/(far-near), 0},
+        {-1*(right+left)/(right-left), -1*(bottom+top)/(bottom-top), -1*near/(far-near), 1}
     }; 
-    matmul_modify(matrix1, rows1, 4, *view, 4, 4);
+
+    matmulModify(matrix1, rows1, 4, *view, 4, 4);
 }
 
-void perstrans(float* matrix1, int rows1, float n, float f){
+void perspectiveTransform(float* matrix1, int rows1, float near, float far){
+    near = -20;
+    far = 20;
     float project[4][4] = {
-        {n, 0, 0, 0},
-        {0, n, 0, 0},
-        {0, 0, (f+n), 1},
-        {0, 0, -1*f*n, 0}
+        {near, 0, 0, 0},
+        {0, near, 0, 0},
+        {0, 0, (far+near), 1},
+        {0, 0, -1*far*near, 0}
     }; //not actually needed because we don't keep track of object stacking
-    matmul_modify(matrix1, rows1, 4, *project, 4, 4);
+    matmulModify(matrix1, rows1, 4, *project, 4, 4);
 }
 
 #endif
--- file_renderer.cpp
+++ file_renderer.cpp
@@ -5,6 +5,10 @@
 #include "CNUMPP_H.h"
 #include "STLREADER_H.h"
 #include "Point3D.h"
+/*
+void render_object(std::string stlFilePath, &Vector3D light_vector, &Vector3D camera_vector,){
+
+}*/
 
 int main() {
     //creates a Bitmap image
@@ -17,28 +21,22 @@ int main() {
     Vector3D camera_vector = Vector3D(0,0,-1);
  
     //import an stl file, process it
-    std::string stlFilePath = "Dodecahedron-converted-ASCII.stl";
+    
+    std::string stlFilePath = ".stl_files/Dodecahedron-converted-ASCII.stl";
     std::string stl_string = readFileIntoString(stlFilePath);
 
-    //Datastructure used to store coordinates
+    //d atastructure used to store coordinates
     std::vector<std::array<Vector3D, 4>> stl_data;
 
     //parses raw STL string, appends data to STL_data
     parseSTL(stl_string, stl_data);
 
-    //coordinates that help define the rerndering box
-    int left = -20;
-    int bottom = -20;
-    int near = -20;
-
-    //Coordinates that help define the rendering box
-    int right = 20;
-    int top = 20;
-    int far = 20;
+    Vector3D bottom_near_left_point = Vector3D(-20,-20,-20);
+    Vector3D top_far_right_point = Vector3D(20,20,20);
 
     for(std::array<Vector3D, 4> face : stl_data){
 
-        //converts Raw coordinates into an array, to be processed by functions
+        //converts raw coordinates into an array, to be processed by functions
         float vertex_array[3][4] = 
            {{face[1].x,face[1].y,face[1].z,1},
             {face[2].x,face[2].y,face[2].z,1},
@@ -49,17 +47,17 @@ int main() {
 
         //modifies the vertex array coordinates [in the case of rotation, changes the facet vector too]
         //may change such that the camera moves instead
-        xrotate(*vertex_array, 3, 45, facet_vector);
-        yrotate(*vertex_array, 3, 45, facet_vector);
-        zrotate(*vertex_array, 3, 45, facet_vector);
+        //facetRotationX(*vertex_array, 3, 45, facet_vector);
+        //facetRotationY(*vertex_array, 3, 45, facet_vector);
+        //facetRotationZ(*vertex_array, 3, 45, facet_vector);
 
-        //fit object, and add depth 
-        perstrans(*vertex_array, 3, near, far);
-        viewvol(*vertex_array, 3, right, left, bottom, top, far, near);
+        //fit object, and add depth
+        
+        //viewVolumeTransform(*vertex_array, 3, bottom_near_left_point, top_far_right_point);
 
         //weights, used to divide the x.y coordinates for depth
         float w1 = vertex_array[0][2]; float w2 = vertex_array[1][2]; float w3 = vertex_array[2][2];
-
+        w1 = 1; w2 = 1; w3 = 1;
         //shade calculated by dot product to light vector
         float shade = dotproduct(facet_vector,light_vector)/3.14195;
         shade = 1-shade;
@@ -69,9 +67,12 @@ int main() {
         Vector3D point2 = Vector3D(vertex_array[1][0]/w2,vertex_array[1][1]/w2,vertex_array[1][3]);
         Vector3D point3 = Vector3D(vertex_array[2][0]/w3,vertex_array[2][1]/w3,vertex_array[2][3]);
 
+        std::cout << point1 << " " << point2 << " " << point3 << std::endl;
+
         //draws Facets
-        bitmap.drawallFacet(point1, point2, point3, 256*shade,0,0);
-        //bitmap.drawFacetedge(point1, point2, point3, 255,255,255);
+        //bitmap.drawallFacet(point1, point2, point3, 256*shade,0,0);
+
+        bitmap.drawFacetedge(point1, point2, point3, 255,255,255);
     }
     //saves bitmap
     bitmap.save("output.bmp");
